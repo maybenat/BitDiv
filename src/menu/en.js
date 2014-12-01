fdbb4826d511719976c630b5d9f4a69817386424
Natalie McMullen <natalie.mcmullen@utah.edu>
2014-12-01 11:22:41 -0700

    Initial Commit

---
+++ src/menu/en.js
@@ -0,0 +1,59 @@
+{
+
+    "header": {
+        "navbar": {
+            "UPLOAD": "Upload",
+            "new": {
+                "NEW": "New",
+                "PROJECT": "Projects",
+                "USER": "User"
+            },
+            "NOTIFICATIONS": "Notifications"
+        }
+    },
+    "aside": {
+        "nav": {
+            "HEADER": "Navigation",
+            "DASHBOARD": "Dashboard",
+            "CALENDAR": "Calendar",
+            "EMAIL": "Email",
+            "WIDGETS": "Widgets",
+            "components": {
+                "COMPONENTS": "Research & Learn",
+                "ui_kits": {
+                    "UI_KITS": "UI Kits",
+                    "BUTTONS": "Buttons",
+                    "ICONS": "Icons",
+                    "GRID": "Grid",
+                    "BOOTSTRAP": "Bootstrap",
+                    "SORTABLE": "Sortable",
+                    "PORTLET": "Portlet",
+                    "TIMELINE": "Timeline"
+                },
+
+                "CHART": "Stock Example Page",
+                "VECTOR_MAP": "World View",
+
+                "pages": {
+                    "PAGES": "Pages",
+                    "PROFILE": "Profile",
+                    "POST": "Post",
+                    "SEARCH": "Search",
+                    "INVOICE": "Invoice",
+                    "LOCK_SCREEN": "Lock screen",
+                    "SIGNIN": "Signin",
+                    "SIGNUP": "Signup",
+                    "FORGOT_PASSWORD": "Forgot password",
+                    "404": "404"
+                }
+            },
+            "your_stuff": {
+                "YOUR_STUFF": "Your Stuff",
+                "PROFILE": "Profile",
+                "DOCUMENTS": "Help"
+            }
+        },
+        "MILESTONE": "Investment Progress"
+    }
+
+}
