72f6cb3f26e9a2ac0cec134d47c0f8d3b10b6a58
Natalie McMullen <natalie.mcmullen@utah.edu>
2015-02-10 14:40:18 -0700

    Fixed header thing

--- BitDiv_Stripped/html/index.php
+++ BitDiv_Stripped/html/index.php
@@ -241,8 +241,7 @@
 
         </div>
 
-        <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-        <script src="../bower_components/bootstrap/dist/js/bootstrap.js"></script>
+        
         <script src="js/ui-load.js"></script>
         <script src="js/ui-jp.config.js"></script>
         <script src="js/ui-jp.js"></script>
