95cc2756b9d76731a95a9d5833e302e3dc783d8d
GuanxiongWu <bryantxiong@gmail.com>
2015-03-11 19:04:15 -0600

    show list of people in DB

---
+++ BitDiv_Stripped/html/peopleDB.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'data/config.php';
+
+if(true) {
+    try {
+        $db = new PDO ( "mysql:host=$host;dbname=$dbname;charset=utf8", $user, $dbPassword );
+        $db->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $db->setAttribute ( PDO::ATTR_EMULATE_PREPARES, false );
+
+        if(true) {
+            session_name('Private');
+            $email = $_SESSION['email'];
+            $statement = $db->prepare("SELECT * FROM users WHERE email != '$email'");
+            $statement->execute();
+
+            $peopleList = array();
+            $riskList = array();
+
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            foreach($result as $row)
+            {
+                $peopleList[$row['email']] = $row['first_name'] . " " . $row['last_name'];
+                $riskList[$row['email']] = $row['risk'];
+            }
+        }
+        $db = null;
+    } catch(PDOException $e) {
+        echo '<script language="javascript"> alert("Unable to connect to the database111") </script>';
+    }
+}
+?>
