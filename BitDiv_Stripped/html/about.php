79a94c6090458a63d157b50ca8f5e600936754c2
Natalie McMullen <natalie.mcmullen@utah.edu>
2015-04-23 10:56:59 -0600

    Added my About me

--- BitDiv_Stripped/html/about.php
+++ BitDiv_Stripped/html/about.php
@@ -20,25 +20,33 @@
       <a href="index.php"><img src="img/logoy.png"></a>
       <div class="m-b-lg">
         <div class="content wrapper text-center">
-          <p class="m-n font-thin">Project Description here.</p>
+          <p class="m-n">bitdiv is an investment 
+tracking and planning website. 
+It allows users to analyze their portfolios
+based on financial metrics, news sources, 
+and even other users. We seek to get investors quickly informed
+about investments through a series of 
+visualizations that can be compared across 
+different stocks. The emphasis is on helping the
+user make a choice they feel good about.</p>
           <hr />
         </div>
           <p class="m-n font-thin"><strong>Team Members</strong></p>
           <ul>
           <p class="m-n font-thin">Natalie McMullen</p>
-          <p class="m-n font-thin">paragraph</p>
-          <p class="m-n font-thin"><small><a href="">Website</a> | <a href="">LinkedIn</a></small></p>
+          <p class="m-n ">Interested in software engineering, full stack development, data analytics and web development.</p>
+          <p class="m-n font-thin"><small><a href="http://eng.utah.edu/~mcmullen/">Website</a> | <a href="https://www.linkedin.com/pub/natalie-mcmullen/64/696/ab8">LinkedIn</a></small></p>
           <hr />
           <p class="m-n font-thin">Michael Scutt</p>
-          <p class="m-n font-thin">Interests in embedded development, computer engineering, and signal processing.</p>
+          <p class="m-n ">Interests in embedded development, computer engineering, and signal processing.</p>
           <p class="m-n font-thin"><small><a href="http://www.linkedin.com/in/mscutt">Website</a> | <a href="http://www.linkedin.com/in/mscutt">LinkedIn</a></small></p>
           <hr />
           <p class="m-n font-thin">Karl Tharp</p>
-          <p class="m-n font-thin">paragraph</p>
+          <p class="m-n ">paragraph</p>
           <p class="m-n font-thin"><small><a href="">Website</a> | <a href="">LinkedIn</a></small></p>
           <hr />
           <p class="m-n font-thin">Guanxiong Wu</p>
-          <p class="m-n font-thin">paragraph</p>
+          <p class="m-n ">paragraph</p>
           <p class="m-n font-thin"><small><a href="">Website</a> | <a href="">LinkedIn</a></small></p>
           </ul>
           <hr />
